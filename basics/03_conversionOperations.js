9755d5c9301e571fe69fa9449b4da756d5fd273e
Harshvardhan <121446100+LastMinuteLion@users.noreply.github.com>
2023-12-04 18:23:59 +0000

    conversions

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,62 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+
+console.log(typeof (valueInNumber));
+//Nan =>  Not a Number.
+
+//score is 33abs it will return Nan
+
+//when the score is null , then it will it convert it to 0 which is a anomaly.
+
+//If empty string is converted to boolean , it will return false , vice versa TRUE.
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+/**************************************************OPERATIONS*********************************************** */
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
