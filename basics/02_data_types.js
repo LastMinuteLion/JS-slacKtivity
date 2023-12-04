e8b3b48483407edf76d97a51cad75fb5bb5df94e
Harshvardhan <121446100+LastMinuteLion@users.noreply.github.com>
2023-12-04 16:02:39 +0000

    /date types

---
+++ basics/02_data_types.js
@@ -0,0 +1,14 @@
+"use strict" ; //treat all JS code as newer versions
+
+//alert(3+3) // We are using nodejs , not browser 
+let name = "Harsh"
+let age = 18
+let isLoggedIn = true
+
+//undefined => 
+//symbol => unique
+
+// NULL is a standalone value   eg if temp is coming from API 0 , 0 is a temperature thats why we show NULL , it states that the temp isn't received.
+
+console.log(typeof null); //it is a object 
+console.log(typeof undefined);
